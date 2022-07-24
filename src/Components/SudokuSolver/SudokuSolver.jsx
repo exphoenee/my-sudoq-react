bb7c4d221c0aac39a37e803bd91efd8c4a3e7a03
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-24 14:43:39 +0200

    Made the solver

---
+++ src/Components/SudokuSolver/SudokuSolver.jsx
@@ -0,0 +1,15 @@
+/* Libraries */
+import React from "react";
+
+/* Other imports */
+
+/* Components */
+
+export default function SudokuSolver() {
+  return (
+    <div>
+      <h1>SudoQ Solver</h1>
+    </div>
+  );
+}
+/* Styled Components */
