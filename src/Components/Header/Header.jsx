2e4bc21332a9cd99826a0b1d494b102cd6109259
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-24 14:42:02 +0200

    Create a nice header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,31 @@
+/* Libraries */
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+/* Components */
+
+export default function Header({ menuPoints }) {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Menu</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {menuPoints.map((elem) => (
+              <Link to={elem.route} className="nav-link">
+                {elem.text}
+              </Link>
+            ))}
+          </Nav>
+          <Nav>
+            <Nav.Link href="">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+/* Styled Components */
