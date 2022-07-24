21bd2feff5cda80d7a13400b334301d844b354fd
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-24 14:43:50 +0200

    Dummy landing page

---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+/* Libraries */
+import React from "react";
+
+/* Other imports */
+
+/* Components */
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
+}
+/* Styled Components */
