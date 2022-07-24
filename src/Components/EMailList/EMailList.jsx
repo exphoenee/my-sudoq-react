9441432805fffe3016580b7800971df2cf3626ca
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-24 17:53:53 +0200

    email list is also done

--- src/Components/EMailList/EMailList.jsx
+++ src/Components/EMailList/EMailList.jsx
@@ -1,49 +1,91 @@
 /* Libraries */
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
+import axios from "axios";
 
 /* Other imports */
 
 /* Components */
 
 export default function EMailList() {
+  const success = "success";
+  const danger = "danger";
+  const warning = "warning";
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [age, setAge] = useState("");
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState({ text: "", type: "" });
   const [accept, setAccept] = useState(false);
+
+  const getMockData = () => {
+    axios
+      .get(
+        `https://jsonplaceholder.typicode.com/users/${Math.floor(
+          Math.random() * 10
+        )}`
+      )
+      .then((res) => {
+        setName(res.data.name);
+        setEmail(res.data.email);
+        setAge(res.data.address.suite);
+      });
+  };
+
+  useEffect(() => {
+    getMockData();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      let res = await fetch("https://httpbin.org/post", {
-        method: "POST",
-        body: JSON.stringify({
-          name,
-          email,
-          age,
-        }),
+
+    if (accept) {
+      axios
+        .post(`https://jsonplaceholder.typicode.com/users/`, {
+          name: name,
+          username: "don't we have",
+          email: email,
+          address: {
+            street: "Seesam",
+            suite: age,
+            city: "London",
+            zipcode: "1024",
+            geo: {
+              lat: "24.8918",
+              lng: "21.8984",
+            },
+          },
+          phone: "+36306106608",
+          website: "https://bozzayviktor.hu",
+          company: {
+            name: "CubicFox",
+            catchPhrase: "Gimme that job!",
+            bs: "generate enterprise e-tailers",
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.status === 200 || res.status === 201) {
+            setMessage({ text: "User created successfully", type: success });
+          } else {
+            setMessage({ text: "Some error occured", type: danger });
+          }
+        });
+    } else {
+      setMessage({
+        text: "Please check the chckbox to accept everything",
+        type: warning,
       });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setName("");
-        setEmail("");
-        setMessage("User created successfully");
-      } else {
-        setMessage("Some error occured");
-      }
-    } catch (err) {
-      console.log(err);
     }
   };
 
   return (
-    <Container style={containerStyle} className=" container-sm w-50">
+    <Container style={containerStyle} className="container-sm w-50">
       <h1>Subscribe for our email list</h1>
       <Form onSubmit={handleSubmit}>
         <Row className="mb-3">
@@ -86,6 +128,7 @@ export default function EMailList() {
             label="I accept anything just send me a lot of useless information every day"
           />
         </Form.Group>
+        <Alert variant={message.type}>{message.text}</Alert>
         <Button variant="primary" type="submit">
           Send
         </Button>
