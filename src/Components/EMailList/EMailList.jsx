31962ecb400dbf808c5c3cf033fefaeeca38ef62
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-24 15:18:04 +0200

    Making Form to working

--- src/Components/EMailList/EMailList.jsx
+++ src/Components/EMailList/EMailList.jsx
@@ -1,5 +1,6 @@
 /* Libraries */
 import React from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
@@ -11,29 +12,76 @@ import Col from "react-bootstrap/Col";
 /* Components */
 
 export default function EMailList() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [message, setMessage] = useState("");
+  const [accept, setAccept] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          email,
+          age,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setName("");
+        setEmail("");
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Container className="mt-5 w-50">
+    <Container style={containerStyle} className=" container-sm w-50">
       <h1>Subscribe for our email list</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Row className="mb-3">
           <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter your name" />
+            <Form.Control
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              placeholder="Enter your name"
+            />
           </Form.Group>
           <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
             <Form.Label>Age</Form.Label>
-            <Form.Control type="number" placeholder="Enter your age" />
+            <Form.Control
+              onChange={(e) => setAge(e.target.value)}
+              value={age}
+              type="number"
+              placeholder="Enter your age"
+            />
           </Form.Group>
         </Row>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder="Enter email"
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check
+            onChange={(e) => setAccept(e.target.checked)}
+            value={accept}
             type="checkbox"
             label="I accept anything just send me a lot of useless information every day"
           />
@@ -46,4 +94,5 @@ export default function EMailList() {
   );
 }
 
-/* Styled Components */
+/* Styles and Styled Components */
+const containerStyle = { minHeight: "650px", marginTop: "100px" };
