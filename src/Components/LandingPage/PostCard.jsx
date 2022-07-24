b653f668563981d9dbe91659795983610ad99b36
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-24 17:53:19 +0200

    making the cards fancier
    I need help to get random image form piscum...

--- src/Components/LandingPage/PostCard.jsx
+++ src/Components/LandingPage/PostCard.jsx
@@ -1,10 +1,25 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
+/*images*/
+import img1 from "../../media/img1.jpeg";
+import img2 from "../../media/img2.webp";
+import img3 from "../../media/img3.jpeg";
+import img4 from "../../media/img4.webp";
+import img5 from "../../media/img5.webp";
+import img6 from "../../media/img6.webp";
+import img7 from "../../media/img7.jpg";
+
+const images = [img1, img2, img3, img4, img5, img6, img7];
+
 export default function PostCard({ title, description }) {
   return (
     <Card style={cardStyle}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Img
+        variant="top"
+        className="w-100 mt-3 rounded-2"
+        src={images[Math.floor(Math.random() * images.length)]}
+      />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
