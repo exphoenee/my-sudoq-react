9dd68ad2adbf9523314d929f4076c01792aa5691
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-25 11:17:07 +0200

    made landingpage fancier
    After lunch I will continue....

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
 import { v4 as uuidv4 } from "uuid";
-import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
 
 /* Other imports */
 
@@ -12,9 +12,13 @@ import Row from "react-bootstrap/Row";
 import PostCard from "./PostCard";
 
 export default function LandingPage() {
+  const paginationRule = [1, 5, 10, 25, 50];
+
   const [mockPosts, setMockPosts] = useState([{}]);
   //const [mockImage, setMockImage] = useState();
-  //const [page, setPage] = useState();
+  const [page, setPage] = useState(1);
+  const [postsOnPage, setPostsOnPage] = useState(paginationRule[1]);
+  const pageNr = Math.ceil(mockPosts.length / postsOnPage);
 
   const getPosts = async () => {
     axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
@@ -37,23 +41,70 @@ export default function LandingPage() {
   useEffect(() => {
     getPosts();
     //getImage();
-  }, []);
+  }, [postsOnPage]);
 
   return (
     <Container className="mt-5 container-md w-75">
-      <h1 className="text-center">Landing Page</h1>
-      {mockPosts
-        .filter((post, i) => post.id <= 5)
-        .map((post) => (
-          <Row key={uuidv4()}>
+      <h1 className="">Landing Page</h1>
+      <div className="d-flex flex-column justify-content-center align-content-center mt-5">
+        <div id="control-board" className="btn-group w-25 mx-auto" role="group">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => setPage(page > 1 ? page - 1 : page)}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => setPage(page < pageNr ? page + 1 : page)}
+          >
+            Next
+          </button>
+        </div>
+        <p className="text-center mb-0">
+          Page {page} form {pageNr}
+        </p>
+        <p className="text-center">
+          there are {mockPosts.length} posts to read
+        </p>
+        <Form.Group className="mb-3" controlId="ControlInput1">
+          <Form.Select
+            className="w-25 mx-auto text-center"
+            size="sm"
+            onChange={(e) => {
+              const newPPP = +e.target.value;
+              setPage(Math.ceil((postsOnPage * page) / newPPP));
+              setPostsOnPage(newPPP);
+            }}
+          >
+            {paginationRule.map((p) => (
+              <option
+                key={uuidv4()}
+                selected={p === postsOnPage ? true : false}
+                value={p}
+              >
+                {p} post{p > 1 ? "s" : ""} / page
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </div>
+      <div className="d-flex flex-row  flex-wrap justify-content-center">
+        {[...mockPosts]
+          .splice((page - 1) * postsOnPage, postsOnPage)
+          .map((post) => (
             <PostCard
               title={post.title}
               description={post.body}
               key={uuidv4()}
             />
-          </Row>
-        ))}
+          ))}
+      </div>
     </Container>
   );
 }
+//<Row key={uuidv4()}>
+//</Row>
 /* Styled Components */
